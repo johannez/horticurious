20e769c48898af568ff72ca8198113956461c5e2
Johannes Schmidt <mail@johannez.com>
2018-09-05 21:31:35 -0700

    favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
     <div class="content">
         <div id="logo">
-            <img src="{!! asset('svg/logo.svg') !!}" alt="logo">
+            <img src="/svg/logo.svg" alt="logo">
         </div>
 
         <h2>Coming soon...</h2>
