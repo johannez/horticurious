af4145936ed6771632c822658d4bd12bd560b895
Johannes Schmidt <mail@johannez.com>
2020-05-13 22:31:18 -0700

    Implemented testimonial content type

---
+++ app/Nova/Testimonial.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Fields\DateTime;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Testimonial extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Testimonial';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Trix::make('Quote')
+                ->hideFromIndex(),
+
+            Text::make('Author')
+                ->sortable()
+                ->required(true),
+                
+            DateTime::make('Created At')
+                ->sortable()
+                ->hideWhenUpdating()
+                ->hideWhenCreating()
+                ->hideFromIndex()
+                ->format('M/D/YYYY - h:mma'),
+
+            DateTime::make('Updated At')
+                ->format('M/D/YYYY - h:mma'),        
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
