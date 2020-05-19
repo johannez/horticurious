794ca5cfc11629330a4361e2c5b987496aa11b4c
Johannes Schmidt <mail@johannez.com>
2020-05-18 20:08:47 -0700

    nova permissions

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -82,7 +82,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'mail@johannesschmidt.ca',
+                'trisha@horticurious.ca'
             ]);
         });
     }
